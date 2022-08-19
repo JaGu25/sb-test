f186a5331cca3115154b7a16d3ddbea3946ef591
JaGu25 <albitestapia25@gmail.com>
2022-08-19 07:04:38 -0500

    first commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,40 @@
+import './myLabel.css'
+
+export interface MyLabelProps {
+    /**
+    * Este es el mensaje a mostrar
+    */
+    label: string;
+    /**
+    * Este es el tamaño de la etiqueta
+    */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+    * Todo capitalizado
+    */
+    allCaps: boolean;
+    /**
+     * Colores basico del texto
+     */
+    color: 'primary' | 'secondary' | 'tertiary'
+    /**
+    * Custom Color
+    */
+    fontColor?: string
+}
+
+const MyLabel = ({
+    label = "No Label",
+    size = "normal",
+    color = "primary",
+    allCaps = false,
+    fontColor
+}: MyLabelProps) => {
+    return (
+        <span style={{ color: fontColor }} className={`label ${size} text-${color}`}>
+            {allCaps ? label.toUpperCase() : label}
+        </span>
+    )
+}
+
+export default MyLabel
